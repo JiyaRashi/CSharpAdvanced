9de65384cc05da5c051fe01afe17664f7f0324de
Jiyavudeen r <jiyavudeen.r379@outlook.com>
2021-06-23 14:32:01 +0530

    moved to separate class

---
+++ InterfacevsAbstract.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced
+{
+     public class InterfacevsAbstract
+    {
+        public abstract class customer
+        {
+
+            public abstract void discount();
+
+        }
+
+        public abstract class customernoDiscount
+        {
+            public string name;
+
+        }
+        public interface Icustomer_1
+        {
+            string name { get; set; }
+
+        }
+
+        public interface Icustomer_2
+        {
+            void discount();
+
+        }
+
+        public class PlantinumCustomer_abstract : customer
+        {
+            public override void discount()
+            {
+                Console.WriteLine("50 discount abs");
+            }
+        }
+
+        public class SilverCustomer_abstract : customer
+        {
+            public override void discount()
+            {
+                Console.WriteLine("30 discount abs");
+            }
+        }
+
+        public class GoldCustomer_abstract : customer
+        {
+            public override void discount()
+            {
+                Console.WriteLine("40 discoun abs");
+            }
+        }
+
+        public class NormalCustomer
+        {
+            // no discount 
+            //  if inherit abstract class ,In situvation need develop discount method in normal class
+        }
+
+        public class PlantinumCustomer_Interface : Icustomer_2
+        {
+            public void discount()
+            {
+                Console.WriteLine("50 discount interface");
+            }
+        }
+
+        public class SilverCustomer_Interface : Icustomer_2
+        {
+            public string name = "Silver Interface";
+
+            public void discount()
+            {
+                Console.WriteLine("30 discount interface");
+            }
+        }
+
+        public class NormalCustomer_Interface : Icustomer_1
+        {
+            public string name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CSharpAdvanced.InterfacevsAbstract;
 
 namespace CSharpAdvanced
 {
@@ -30,89 +31,12 @@ namespace CSharpAdvanced
 
             SilverCustomer_Interface sc_interface = new SilverCustomer_Interface();
             sc_interface.discount();
-            sc_interface.name = "Silver"; 
+            sc_interface.name = "Silver";
             #endregion
 
             Console.ReadKey();
 
 
-        }
-
-        public abstract class customer
-        {
-
-            public abstract void discount();
-            
-        }
-
-        public abstract class customernoDiscount
-        {
-            public string name;
-
-        }
-        public interface Icustomer_1
-        {
-            string name { get; set; }
-
-        }
-
-        public interface Icustomer_2
-        {
-            void discount();
-
-        }
-
-        public class PlantinumCustomer_abstract : customer
-        {
-            public override void discount()
-            {
-                Console.WriteLine("50 discount abs");
-            }
-        }
-
-        public class SilverCustomer_abstract : customer
-        {
-            public override void discount()
-            {
-                Console.WriteLine("30 discount abs");
-            }
-        }
-
-        public class GoldCustomer_abstract : customer
-        {
-            public override void discount()
-            {
-                Console.WriteLine("40 discoun abs");
-            }
-        }
-
-        public class NormalCustomer
-        {
-            // no discount 
-            //  if inherit abstract class ,In situvation need develop discount method in normal class
-        }
-
-        public class PlantinumCustomer_Interface : Icustomer_2
-        {
-            public void discount()
-            {
-                Console.WriteLine("50 discount interface");
-            }
-        }
-
-        public class SilverCustomer_Interface : Icustomer_2
-        {
-            public string name  = "Silver Interface";
-
-            public void discount()
-            {
-                Console.WriteLine("30 discount interface");
-            }
-        }
-
-        public class NormalCustomer_Interface : Icustomer_1
-        {
-            public string name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         }
     }
 }
