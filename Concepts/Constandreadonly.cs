f030706c6d260e27483abbd6e47cb1baefd91f50
Jiyavudeen RAJMOHAMED <jiyavudeen.rajmohamed@alstomgroup.com>
2023-02-02 16:09:46 +0530

    Added more ex

--- Concepts/Constandreadonly.cs
+++ Concepts/Constandreadonly.cs
@@ -8,47 +8,65 @@ namespace CSharpAdvanced
 {
    public class Constandreadonly
     {
-        const int c = 100;// need to provide value at time declartion 
-        //c =150;
-        readonly string _oldVal;
+        const string constval_old = "const old vlaue";
+        const string  constval_new = "const new value";
 
-        readonly static string Val;
-        readonly static string Val1;
+
+
+        // need to provide value at time declartion 
+        //c =150;
+        readonly string _oldVal = "Prefix";
+        readonly string _newVal = "New Value";
+
+        readonly static string stativVal_old;
+        readonly static string stativVal_new;
 
         // s="This string";
         public Constandreadonly()
         {
-            Console.WriteLine($"======Constandreadonly ========");
+            Console.WriteLine($"======Public ===readonly ================");
 
             // Val = "Hey static readonly";
             //c = 123;
-            string _newVal = "New Value ";
-            _oldVal= "Old Value";
+           
+           // _oldVal= "Old Value";
             _oldVal = _newVal;
-            Console.WriteLine($"-------Assigned Value :_oldVal = _newVal-----");
-            Console.WriteLine($"------_oldVal: {_oldVal}");
+            Console.WriteLine($"-ctor------Assigned Value :_oldVal = _newVal-----");
+            Console.WriteLine($"-ctor-----_oldVal: {_oldVal}");
 
-        }
+            Console.WriteLine($"======Public ===Constant ================");
+            //constval_old = "";
+           // const string constval_olddd = "";
 
-
-        public void Method()
-        {
-          // s = "This string";
         }
 
         static Constandreadonly()
         {
-            Val = "Hey static readonly";
-            Val1 = "again readonly static";
-            Val = Val1;
-            Console.WriteLine($"-------static Ctor------->{Val}");
+            Console.WriteLine($"======static ===readonly ================");
+
+            stativVal_old = "old static value";
+            stativVal_new = "new static value";
+            stativVal_old = stativVal_new;
+            Console.WriteLine($"-ctor------Assigned Value :stativVal_old = stativVal_new-----");
+            Console.WriteLine($"-ctor------static Ctor------->{stativVal_old}");
 
         }
         public Constandreadonly(string val)
         {
+            Console.WriteLine($"=========Constandreadonly ======method Param==========");
+
             //Val = "dfdf";
-            _oldVal = "This string" + val;
+            _oldVal = $"This string { val}";// ex 1
+            _oldVal = $"{_oldVal} This string { val}";// ex 2
+
+            
             Console.WriteLine($"-------Ctor with Parameter------->{_oldVal}");
         }
+
+
+        //==============================Points to rember ===============================
+           // const value cannt be changed 
+           // readonly value can be changed  using ctor
+           // static readonly can be changes using static ctor
     }
 }
